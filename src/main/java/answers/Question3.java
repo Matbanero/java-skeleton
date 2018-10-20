de0c4eda4b432306a586ebe1776b438357bd9b9e
Matbanero <mateusz.meller2@gmail.com>
2018-10-20 14:42:25 +0100

    Added solution to the third question.

--- src/main/java/answers/Question3.java
+++ src/main/java/answers/Question3.java
@@ -5,8 +5,31 @@ import helpers.Edge;
 public class Question3 {
 
 	public static int lowestExposureToExchanges(int numNodes, Edge[] edgeList) {
-		// TODO Auto-generated method stub
-		return -1;
+		int X = numNodes;
+		int Y = 0;
+		int biggestFrequency = 0;
+		int [] frequency = new int[numNodes];
+
+		for (int i = 1; i <= numNodes; i++) {
+			int counter = 0;
+			for (Edge edge : edgeList) {
+				if (edge.getEdgeA() == i || edge.getEdgeB() == i) {
+					counter++;
+				}
+			}
+			if (counter > biggestFrequency) {
+				biggestFrequency = counter;
+			}
+			frequency[i - 1] = counter;
+		}
+		for (int freq : frequency) {
+			if (freq == biggestFrequency) {
+				X--;
+				Y++;
+			}
+		}
+
+		return X - Y;
 	}
 
 }
