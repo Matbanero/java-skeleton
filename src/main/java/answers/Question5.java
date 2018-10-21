4a0b6ab459c43fc9b9c47529e13c8f527e14b1d5
Matbanero <mateusz.meller2@gmail.com>
2018-10-21 21:14:26 +0100

    Added solution to the fifth question.

--- src/main/java/answers/Question5.java
+++ src/main/java/answers/Question5.java
@@ -1,10 +1,88 @@
 package answers;
+import java.util.ArrayList;
 
 public class Question5 {
 
 	public static int shareExchange(int[] allowedAllocations, int totalValue) {
-		// TODO Auto-generated method stub
-		return -1;
+		int maxVal = 0;
+		int minVal = totalValue;
+		for (int i = 0; i < allowedAllocations.length; i++) {
+			if (allowedAllocations[i] > maxVal) {
+				maxVal = allowedAllocations[i];
+			}
+
+			if (allowedAllocations[i] < minVal) {
+				minVal = allowedAllocations[i];
+			}
+		}
+
+		System.out.println("Max weight: " + maxVal + "\nMin weight: " + minVal);
+
+		
+		int n = allowedAllocations.length;
+		int w[] = new int[allowedAllocations.length + 1];
+		int p[] = new int[allowedAllocations.length + 1];
+		
+		for (int i = 1; i <= allowedAllocations.length; i++) {
+			w[i] = allowedAllocations[i - 1];
+			p[i] = allowedAllocations[i - 1];
+		}
+
+		int g[] = new int[totalValue + maxVal + 1];
+		int d[] = new int[totalValue + maxVal + 1];
+
+		for (int i = 1; i <= totalValue + maxVal; i++) {
+			g[i] = 0;
+			d[i] = n;
+		}
+
+
+		for (int i = 1; i <= n; i++) {
+			if (g[w[i]] < p[i]) {
+				g[w[i]] = p[i];
+				d[w[i]] = i;
+			}
+		}
+
+		int optimal = 0;
+		int optCap = 0;
+		for (int y = minVal; y <= totalValue; y++) {
+			if (g[y] <= optimal) {
+			 	continue;
+			}
+
+			optimal = g[y];
+			optCap = y;
+			for (int i = 1; i <= d[y]; i++) {
+				if (g[y + w[i]] < g[y] + p[i]) {
+					g[y + w[i]] = g[y] + p[i];
+					d[y + w[i]] = i;
+				}
+			}
+
+			System.out.println("\nOptimal solution: " + optimal);
+			System.out.println('\n');
+			for (int k = 0; k < g.length; k++) {
+				System.out.println(k + ": " + g[k]);
+			}
+			System.out.println('\n');
+
+			for (int k = 0; k < g.length; k++) {
+				System.out.println(k + ": " + d[k]);
+			}
+			System.out.println('\n');
+		}
+
+		
+		int i = d[optCap];
+		ArrayList<Integer> sol = new ArrayList<>();
+		int solSum = 0;
+		while (solSum != optimal) {
+			solSum += w[i];
+			sol.add(w[i]);
+			i = d[optCap - w[i]];
+		}
+		return sol.size();
 	}
 
 }
--- src/main/java/answers/Question6.java
+++ src/main/java/answers/Question6.java
@@ -1,10 +1,60 @@
 package answers;
 
+
+// TODO: COME BACK HERE!!!!
+
 public class Question6 {
 
 	public static int shortestServerRoute(int numServers, int targetServer, int[][] times) {
-		// TODO Auto-generated method stub
-		return -1;
+		int shortestDistance = Integer.MAX_VALUE;
+		int dist[] = new int[numServers];
+		boolean shortesPathVisited[] = new boolean[numServers];
+		dist[0] = 0;
+		shortesPathVisited[0] = false;
+
+		// Initializing the distances and set of shorthest paths
+		for (int i = 1; i < numServers; i++) {
+			if (times[i] != times[0]) {
+				// Set distance to "infinity", it represents that we don't know real distance
+				// from source to given server
+				dist[i] = Integer.MAX_VALUE;
+				shortesPathVisited[i] = false;
+			}
+		}
+
+		for (int i = 0; i < numServers - 1; i++) {
+			// Finds the shortest path to the server (first server in first iteration)
+			int k = minDist(dist, shortesPathVisited);
+			shortesPathVisited[k] = true;
+
+			for (int j = 0; j < numServers; j++) {
+				if (!shortesPathVisited[j] && times[i][j] != 0 
+						&& 	dist[i] + times[i][j] < dist[j]) {
+						dist[j] = dist[i] + times[i][j];
+					}
+				if (dist[j] < shortestDistance) {
+					shortestDistance = dist[j];
+				}
+			}
+		}
+
+		return shortestDistance;
 	}
 
+
+	public static int minDist(int dist[], boolean shortesPathVisited[]) {
+		
+		int min = Integer.MAX_VALUE;
+		// If all paths are visited or there is no smaller distance return -1
+		int min_i = -1;
+
+		for (int i = 0; i < dist.length; i++) {
+			if (shortesPathVisited[i] == false && dist[i] <= min) {
+				min = dist[i];
+				min_i = i;
+			}
+		}
+		
+		return min_i;
+	}
 }
