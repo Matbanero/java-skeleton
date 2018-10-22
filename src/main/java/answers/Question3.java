2372e78abfe19023ec2492b08b8fc0e3eaa74e87
Matbanero <mateusz.meller2@gmail.com>
2018-10-22 21:09:16 +0100

    Fixed solution to the third question.

--- src/main/java/answers/Question3.java
+++ src/main/java/answers/Question3.java
@@ -5,31 +5,52 @@ import helpers.Edge;
 public class Question3 {
 
 	public static int lowestExposureToExchanges(int numNodes, Edge[] edgeList) {
+		int [][] graph = new int[numNodes][numNodes];
+		int [] nodesDegree = new int[numNodes];
 		int X = numNodes;
 		int Y = 0;
-		int biggestFrequency = 0;
-		int [] frequency = new int[numNodes];
 
+		for (Edge e : edgeList) {
+			graph[e.getEdgeA() - 1][e.getEdgeB() - 1] = 1;
+			graph[e.getEdgeB() - 1][e.getEdgeA() - 1] = 1;
+		}
+
+		int topDegree = 0;
 		for (int i = 0; i < numNodes; i++) {
-			int counter = 0;
-			for (Edge edge : edgeList) {
-				if (edge.getEdgeA() == i || edge.getEdgeB() == i) {
-					counter++;
+			for (int j = 0; j < numNodes; j++) {
+				if (graph[i][j] == 1) {
+					nodesDegree[i]++;
 				}
 			}
-			if (counter > biggestFrequency) {
-				biggestFrequency = counter;
-			}
-			frequency[i - 1] = counter;
-		}
-		for (int freq : frequency) {
-			if (freq == biggestFrequency) {
-				X--;
-				Y++;
+			if (nodesDegree[i] > topDegree) {
+				topDegree = nodesDegree[i];
 			}
 		}
 
-		return X - Y;
+		do {
+			for (int i = 0; i < numNodes; i++) {
+				if (nodesDegree[i] == topDegree) {
+					X--;
+					Y++;
+					for (int j = 0; j < numNodes; j++) {
+						graph[i][j] = 0;
+						graph[j][i] = 0;
+						nodesDegree[j]--;
+					}
+					nodesDegree[i] = 0;
+				}
+			}
+
+			topDegree = 0;
+			for (int i = 0; i < numNodes; i++) {
+				if (nodesDegree[i] > topDegree) {
+					topDegree = nodesDegree[i];
+				}
+			}
+
+		} while (topDegree != 0);
+		
+		return X-Y;
 	}
 
 }
