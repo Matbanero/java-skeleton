a5a23b2bd63ac1b067e7fb5ab5874ce5dcd08cf5
Matbanero <mateusz.meller2@gmail.com>
2018-10-24 00:10:47 +0100

    Modified fitfth answer.

--- src/main/java/answers/Question5.java
+++ src/main/java/answers/Question5.java
@@ -4,71 +4,25 @@ package answers;
 public class Question5 {
 
 	public static int shareExchange(int[] allowedAllocations, int totalValue) {
-		int maxVal = 0;
-		int minVal = totalValue;
-		for (int i = 0; i < allowedAllocations.length; i++) {
-			if (allowedAllocations[i] > maxVal) {
-				maxVal = allowedAllocations[i];
-			}
+		int shareBank[] = new int[totalValue + 1];
 
-			if (allowedAllocations[i] < minVal) {
-				minVal = allowedAllocations[i];
-			}
-		}
-		
-		int n = allowedAllocations.length;
-		int w[] = new int[allowedAllocations.length + 1];
-		int p[] = new int[allowedAllocations.length + 1];
-		
-		for (int i = 1; i <= allowedAllocations.length; i++) {
-			w[i] = allowedAllocations[i - 1];
-			p[i] = allowedAllocations[i - 1];
-		}
-
-		int g[] = new int[totalValue + maxVal + 1];
-		int d[] = new int[totalValue + maxVal + 1];
-
-		for (int i = 1; i <= totalValue + maxVal; i++) {
-			d[i] = n;
-		}
-
-
-		for (int i = 1; i <= n; i++) {
-			if (g[w[i]] < p[i]) {
-				g[w[i]] = p[i];
-				d[w[i]] = i;
-			}
-		}
-
-		int optimal = 0;
-		int optCap = 0;
-		for (int y = minVal; y <= totalValue; y++) {
-			if (g[y] <= optimal) {
-			 	continue;
-			}
-
-			optimal = g[y];
-			optCap = y;
-			for (int i = 1; i <= d[y]; i++) {
-				if (g[y + w[i]] < g[y] + p[i]) {
-					g[y + w[i]] = g[y] + p[i];
-					d[y + w[i]] = i;
+		int count = 0;
+		for(int i = 0; i <= totalValue; i++) {
+			for(int j = 0; j < allowedAllocations.length; j++) {
+				if(allowedAllocations[j] <= i) {
+					shareBank[i] = max(shareBank[i], shareBank[i - allowedAllocations[j]] + allowedAllocations[j]);
+				}
+				if(i == totalValue) {
+					count++;
 				}
 			}
 		}
-
-		
-		int i = d[optCap];
-		int count = 0;
-		
-		int solSum = 0;
-		while (solSum != optimal) {
-			solSum += w[i];
-			count++;
-			
-			i = d[optCap - w[i]];
-		}
 		return count;
 	}
+
+	private static int max(int i, int j) {
+		return (i > j) ? i : j;
+	}
+
 
 }
