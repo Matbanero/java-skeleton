a3fb284931219b7ca43666d07d51b5980b5cbfea
Matbanero <mateusz.meller2@gmail.com>
2018-10-24 21:36:31 +0100

    Modified 3rd answer.

--- src/main/java/answers/Question3.java
+++ src/main/java/answers/Question3.java
@@ -1,68 +1,28 @@
 package answers;
-
+import java.util.ArrayList;
 import helpers.Edge;
 
 public class Question3 {
 
 	public static int lowestExposureToExchanges(int numNodes, Edge[] edgeList) {
 
-		
-		int graph[][] = new int[numNodes + 1][numNodes + 1];
+		boolean graph[][] = new boolean[numNodes + 1][numNodes + 1];
+		ArrayList<Integer> setA = new ArrayList<>();
+		ArrayList<Integer> setB = new ArrayList<>();
 
 		// Performing Binary Search on the graph to get minimal value of nodes required to be removed,
 		// to separate all the nodes.
 		for (Edge e : edgeList) {
-			graph[e.getEdgeA()][e.getEdgeB()] = 1;
-			graph[e.getEdgeB()][e.getEdgeA()] = 1;
-		}
-
-		int l = 1;
-		int r = numNodes;
-
-		while (r > l) {
-			int mid = (l + r) >> 1;
-			if (hasRelation(numNodes, mid, edgeList.length, graph) == false) {
-				l = mid + 1;
-			} else {
-				r = mid;
+			graph[e.getEdgeA()][e.getEdgeB()] = true;
+			graph[e.getEdgeB()][e.getEdgeA()] = true;
+			if (!setA.contains(e.getEdgeA()) && !setB.contains(e.getEdgeA())) {
+				setA.add(e.getEdgeA());
+			}
+			if (!setB.contains(e.getEdgeB()) && !setA.contains(e.getEdgeB())) {
+				setB.add(e.getEdgeB());
 			}
 		}
 
-		return numNodes - (2 * l);
-	}
-
-	public static boolean hasRelation(int numNodes, int subsetSize, int numberOfEdges, int[][] graph) {
-		int set = (1 << subsetSize) - 1;
-		int limit = (1 << numNodes);
-
-		int[][] visited = new int[numNodes + 1][numNodes + 1];
-
-		while (set < limit) {
-			visited = new int[numNodes + 1][numNodes + 1];
-
-			int coveredRelations = 0;
-
-			for (int j = 1, e = 1; j < limit; j = j << 1, e++) {
-				if ((set & j) == 1) {
-					// Will mark all the edges (relations) going from this exchange (node)
-					for (int i = 1; i <= numNodes; i++) {
-						if ((graph[e][i] == 1) && (visited[e][i] == 0)) {
-							visited[e][i] = 1;
-							visited[i][e] = 1;
-							coveredRelations++;
-						}
-					}
-				}
-			}
-
-			if (coveredRelations == numberOfEdges) {
-				return true;
-			}
-
-			int previousBit = set & -set;
-			int previousCombination = set + previousBit;
-			set = (((previousCombination ^ set) >> 2) / previousBit) | previousCombination;
-		}	
-		return false;	
+		return setA.size() - setB.size();
 	}
 }
