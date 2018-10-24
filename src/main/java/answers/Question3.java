9e044a88506a37c7dd4bf0d5b4b901a07bd94820
Matbanero <mateusz.meller2@gmail.com>
2018-10-24 22:46:59 +0100

    Modified 3rd answer.

--- src/main/java/answers/Question3.java
+++ src/main/java/answers/Question3.java
@@ -5,16 +5,10 @@ import helpers.Edge;
 public class Question3 {
 
 	public static int lowestExposureToExchanges(int numNodes, Edge[] edgeList) {
-
-		boolean graph[][] = new boolean[numNodes + 1][numNodes + 1];
 		ArrayList<Integer> setA = new ArrayList<>();
 		ArrayList<Integer> setB = new ArrayList<>();
 
-		// Performing Binary Search on the graph to get minimal value of nodes required to be removed,
-		// to separate all the nodes.
 		for (Edge e : edgeList) {
-			graph[e.getEdgeA()][e.getEdgeB()] = true;
-			graph[e.getEdgeB()][e.getEdgeA()] = true;
 			if (!setA.contains(e.getEdgeA()) && !setB.contains(e.getEdgeA())) {
 				setA.add(e.getEdgeA());
 			}
@@ -22,7 +16,6 @@ public class Question3 {
 				setB.add(e.getEdgeB());
 			}
 		}
-
-		return setA.size() - setB.size();
+		return Math.abs(setA.size() - setB.size());
 	}
 }
