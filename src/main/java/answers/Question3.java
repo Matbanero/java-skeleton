8cdf64613cf7d9559c50693749da75b8fb8fdea1
Matbanero <mateusz.meller2@gmail.com>
2018-10-26 19:29:42 +0100

    Modified question 3 solution.

--- src/main/java/answers/Question3.java
+++ src/main/java/answers/Question3.java
@@ -9,13 +9,18 @@ public class Question3 {
 		ArrayList<Integer> setB = new ArrayList<>();
 
 		for (Edge e : edgeList) {
-			if (!setA.contains(e.getEdgeA()) && !setB.contains(e.getEdgeA())) {
+			if (!setA.contains(e.getEdgeA()) && !setB.contains(e.getEdgeA()) && !setA.contains(e.getEdgeB())) {
 				setA.add(e.getEdgeA());
 			}
-			if (!setB.contains(e.getEdgeB()) && !setA.contains(e.getEdgeB())) {
+			if (!setB.contains(e.getEdgeB()) && !setA.contains(e.getEdgeB()) && ! setB.contains(e.getEdgeA())) {
 				setB.add(e.getEdgeB());
 			}
 		}
+
+		if ((setA.size() + setB.size()) < numNodes) {
+			return numNodes - 2 * Math.min(setA.size(), setB.size());
+		}
+
 		return Math.abs(setA.size() - setB.size());
 	}
 }
