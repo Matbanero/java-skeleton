af16ac2d88276098e6be1e279d32d9dfac87580f
Matbanero <mateusz.meller2@gmail.com>
2018-10-29 19:35:57 +0000

    Modified solution 1.

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -1,30 +1,65 @@
 package answers;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class Question1 {
 	public static int bestMergedPortfolio(int[] portfolios) {
-		int bestSolution = 0;
-		int sent = 0;
-		
-		for (int i = 15; i >= 0; i--) {
-			sent = sent | (1 << i);
-			
-			Set<Integer> portfolioSet = new HashSet<>();
-			for (int p : portfolios) {
-				portfolioSet.add(p & sent);
-			}
+		// TODO comment it better
+		return partitionPortoflios(portfolios, 0, portfolios.length - 1, 0, portfolios.length - 1, 0, 30);
+	}
 
-			int temp = bestSolution | (1 << i);
-			for (int pre : portfolioSet) {
-				if (portfolioSet.contains(temp ^ pre)) {
-					bestSolution = temp;
-					break;
-				}
+	private static int partitionPortoflios(int[] portfolios, int lsmall, int lbig, int rsmall, int rbig,
+											int portVal, int bit) {
+		
+		// Base case
+		if (bit < 0) {
+			return portVal;
+		}
+
+		int sent = 1 << bit;
+		int j = lsmall;
+		int m = rsmall;
+
+		for (int i = lsmall; i <= lbig; i++) {
+			if ((portfolios[i] & sent) != 0) {
+				int tempInd = j++;
+				int temp = portfolios[i];
+				portfolios[i] = portfolios[tempInd];
+				portfolios[tempInd] = temp;
 			}
 		}
 
-		return bestSolution;
+		for (int i = rsmall; i <= rbig; i++) {
+			if ((portfolios[i] & sent) != 0) {
+				int tempInd = m++;
+				int temp = portfolios[i];
+				portfolios[i] = portfolios[tempInd];
+				portfolios[tempInd] = temp;
+			}
+		}
+
+		// Recursive case, similar to qsort
+		if (j > lsmall && j <= lbig) {
+			int result = 0;
+			if (m > rsmall) {
+				result = partitionPortoflios(portfolios, j, lbig, rsmall, m-1, portVal * 2 + 1, bit - 1);
+			}
+			if (m <= rbig) {
+				result = Math.max(result, partitionPortoflios(portfolios, lsmall, j-1, m, rbig, portVal * 2 + 1, bit - 1));
+			}
+			return result;
+		} else if (j <= lsmall) {
+			if (m > rsmall) {
+				return partitionPortoflios(portfolios, lsmall, lbig, rsmall, m-1, portVal * 2 + 1, bit - 1);
+			} else {
+				return partitionPortoflios(portfolios, lsmall, lbig, rsmall, rbig, portVal * 2, bit - 1);
+			}
+		} else {
+
+			if (m <= rbig) {
+				return partitionPortoflios(portfolios, lsmall, lbig, m, rbig, portVal * 2 + 1, bit - 1);
+			} else {
+				return partitionPortoflios(portfolios, lsmall, lbig, rsmall, rbig, portVal * 2, bit - 1);
+			}
+		}
+
 	}
 }
