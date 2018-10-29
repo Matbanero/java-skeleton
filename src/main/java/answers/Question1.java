c1611f39a7cd76b18d8bd5f4341231e55d88efd7
Matbanero <mateusz.meller2@gmail.com>
2018-10-29 14:20:23 +0000

    Modified solution 1.

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -1,60 +1,31 @@
 package answers;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Question1 {
-	private static int BIT = 30;
+	private static int BIT = 31;
 	public static int bestMergedPortfolio(int[] portfolios) {
-
-		int n = portfolios.length;
-		int result = 0;
-		Node root = new Node();
-		insertPortfolio(0, root);
-		for (int i = 0; i < n; i++) {
-			result = Math.max(result, query(portfolios[i], root));
-			insertPortfolio(portfolios[i], root);
-		}
-		return result;
-	}
-
-	private static void insertPortfolio(int portfolio, Node r) {
+		int bestSolution = 0;
+		int sent = 0;
+		
 		for (int i = BIT; i >= 0; i--) {
-			int bit = (portfolio & (1 << i));
-			bit = bit > 0 ? 1 : 0;
-			if (r.getChild(bit) == null) {
-				r.setChild(bit);
+			sent = sent | (1 << i);
+			
+			Set<Integer> portfolioSet = new HashSet<>();
+			for (int p : portfolios) {
+				portfolioSet.add(p & sent);
 			}
-			r = r.getChild(bit);
-		}
-	}
 
-	private static int query(int portfolio, Node r) {
-		int comb = 0;
-		for (int i = BIT; i >= 0; i--) {
-			int bit = (portfolio & (1 << i));
-			bit = bit > 0 ? 1 : 0;
-			int notBit = bit == 0 ? 1 : 0;
-			if (r.getChild(notBit) != null) {
-				r = r.getChild(notBit);
-				comb |= (1 << i);
-			} else {
-				r = r.getChild(bit);
+			int temp = bestSolution | (1 << i);
+			for (int pre : portfolioSet) {
+				if (portfolioSet.contains(temp ^ pre)) {
+					bestSolution = temp;
+					break;
+				}
 			}
 		}
-		return comb;
-	}
 
-	private static class Node {
-		private Node[] children = new Node[2];
-		public Node() {
-			this.children[0] = null;
-			this.children[1] = null;
-		}
-
-		public Node getChild(int n) {
-			return this.children[n];
-		}
-
-		public void setChild (int n) {
-			this.children[n] = new Node();
-		}
+		return bestSolution;
 	}
 }
