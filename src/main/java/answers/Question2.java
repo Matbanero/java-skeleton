0e5679c35a2306259039a70609af877f06144ad0
Matbanero <mateusz.meller2@gmail.com>
2018-10-31 20:21:26 +0000

    Modified solution 2.

--- src/main/java/answers/Question2.java
+++ src/main/java/answers/Question2.java
@@ -1,42 +1,28 @@
 package answers;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.stream.Collectors;
 import java.util.ArrayList;
 
 public class Question2 {
 
 	public static int equallyBalancedCashFlow(int[] cashflowIn, int[] cashflowOut) {
-		int minOut = 0;
-		for (int i = 0; i < cashflowOut.length; i++) {
-			if (cashflowOut[i] > minOut) {
-				minOut = cashflowOut[i];
-			}
-		}
-		final int offset = minOut + 1;
+		int sum1 = Arrays.stream(cashflowIn).sum();
+		int sum2 = Arrays.stream(cashflowOut).sum();
 
-		cashflowOut = Arrays.stream(cashflowOut).map(i -> -i).toArray();
-		ArrayList<Integer> total = new ArrayList<>();
-		total.addAll(Arrays.stream(cashflowIn).boxed().collect(Collectors.toList()));
-		total.addAll(Arrays.stream(cashflowOut).boxed().collect(Collectors.toList()));
-		Collections.sort(total);
-		// Shifting numbers to get rid of negatives
-		total.replaceAll(i -> i + offset);
+		Arrays.sort(cashflowIn);
+		Arrays.sort(cashflowOut);
 
-		int sum = total.stream().mapToInt(i -> i).sum();
+		boolean[][] Q = new boolean[cashflowIn.length + 1][sum1 + 1];
+		boolean[][] W = new boolean[cashflowOut.length + 1][sum2 + 1];
 
-		boolean[][] Q = new boolean[total.size() + 1][sum + 1];
-
-		for(int i = 0; i <= sum; i++){
+		for(int i = 0; i <= sum1; i++){
             Q[0][i] = false;
         }
 
-		for (int j = 0; j <= sum; j++) {	
-			for (int i = 1; i <= total.size(); i++) {
+		for (int j = 0; j <= sum1; j++) {	
+			for (int i = 1; i <= cashflowIn.length; i++) {
 				
-				int x = total.get(i - 1);
-				
+				int x = cashflowIn[i - 1];				
 				if (j - x >= 0) {
 					Q[i][j] = Q[i - 1][j] || x == j || Q[i - 1][j - x];
 				} else {		
@@ -45,21 +31,48 @@ public class Question2 {
 			}
 		}
 
-		
-		for (int j = 0; j <= sum; j++) {		
-			for (int i = 0; i < total.size(); i++) {
-				// up to i = total.size - 1 , that is at least one number as used to do the sum
-				int dummy = (total.size() - i) * offset + j;
-				if (dummy > sum) {
-					continue;
-				} else {
-					if (Q[total.size()][dummy]) {
-							return j;
-					}	
+		for(int i = 0; i <= sum2; i++){
+            W[0][i] = false;
+        }
+
+		for (int j = 0; j <= sum2; j++) {	
+			for (int i = 1; i <= cashflowOut.length; i++) {
+				
+				int x = cashflowOut[i - 1];
+				if (j - x >= 0) {
+					W[i][j] = W[i - 1][j] || x == j || W[i - 1][j - x];
+				} else {		
+					W[i][j] = W[i - 1][j] || x == j;
 				}
 			}
 		}
-		return -1;
-	}
 
+		ArrayList<Integer> possibleAns1 = new ArrayList<>();
+		ArrayList<Integer> possibleAns2 = new ArrayList<>();
+
+		for (int i = 0; i <= sum1; i++) {
+			if (Q[cashflowIn.length][i]) {
+				possibleAns1.add(i);
+			}
+		}
+
+		for (int i = 0; i <= sum2; i++) {
+			if (W[cashflowOut.length][i]) {
+				possibleAns2.add(i);
+			}
+		}
+
+		int minDiff = Integer.MAX_VALUE;
+
+		for (Integer ans1 : possibleAns1) {
+			for (Integer ans2 : possibleAns2) {
+				int temp = ans1 - ans2;
+				if (temp >= 0 && temp < minDiff) {
+					minDiff = temp;
+				}
+			}
+		}
+
+		return minDiff;
+	}
 }
